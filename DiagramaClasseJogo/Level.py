5977e657a823a08fff22eb8e18c459ef05863b60
willian aquino <willian_wjunior@hotmail.com>
2024-03-06 15:38:30 -0300

    diagrama-uml

---
+++ DiagramaClasseJogo/Level.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window Surface = None
+        self.entity_list = None
+        self.name = None
+
+    def run(self, ):
+        pass
+
+    def Operation1(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Player.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+from Player import Player
+from Entity import Entity
+from Player import Player
+from Player import Player
+
+class Player(Entity, Player, Entity, Player, Player):
+    def __init__(self):
+        pass
+
+    def Move(self, ):
+        pass
+
